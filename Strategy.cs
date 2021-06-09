9bf85cf64f22e180e070e20b359d68f732cfe53d
JohannKovalenko <johannkovalenko@hotmail.com>
2021-06-09 07:10:33 +0200

    Refactoring 6b - class of strategies

---
+++ Strategy.cs
@@ -0,0 +1,10 @@
+class Strategy
+{
+    public Strategies.Strategy1 one     = new Strategies.Strategy1();
+    public Strategies.Strategy2 two     = new Strategies.Strategy2();
+    public Strategies.Strategy3 three   = new Strategies.Strategy3();
+    public Strategies.Strategy4 four    = new Strategies.Strategy4();
+    public Strategies.Strategy5 five    = new Strategies.Strategy5();
+    public Strategies.Strategy6 six     = new Strategies.Strategy6();
+    public Strategies.Strategy7 seven   = new Strategies.Strategy7();
+}
--- sudoku.cs
+++ sudoku.cs
@@ -12,28 +12,18 @@ class Sudoku
 
     private static OutputData.Sudoku outputData = new OutputData.Sudoku();
 
-    private static Strategies.Strategy1 strategy1 = new Strategies.Strategy1();
-    private static Strategies.Strategy2 strategy2 = new Strategies.Strategy2();
-    private static Strategies.Strategy3 strategy3 = new Strategies.Strategy3();
-    private static Strategies.Strategy4 strategy4 = new Strategies.Strategy4();
-    private static Strategies.Strategy5 strategy5 = new Strategies.Strategy5();
-    private static Strategies.Strategy6 strategy6 = new Strategies.Strategy6();
-    private static Strategies.Strategy7 strategy7 = new Strategies.Strategy7();
-
-
     public static void Main()
     {
-        var stopwatch = new Stopwatch();
+        var stopwatch   = new Stopwatch();
+        var fields      = new Field[10, 10];
+        var strategy    = new Strategy();
 
         stopwatch.Start();
-
-        var fields = new Field[10, 10];
 
         for (int i=1; i<=9; i++)
             for (int j=1; j<=9; j++)
                 fields[i, j] = new Field();
 
-    
         int twotimesnothing = 0;
         int totalFound = 0;
 
@@ -43,7 +33,7 @@ class Sudoku
 
         while(totalFound <81 && twotimesnothing < 2)
         {
-            bool found = Sudoku.ReducePot(fields);
+            bool found = Sudoku.ReducePot(strategy, fields);
 
             if (found)
             {
@@ -64,24 +54,24 @@ class Sudoku
         Console.WriteLine(stopwatch.Elapsed.Milliseconds);
     }
 
-    public static bool ReducePot(Field[,] fields)
+    public static bool ReducePot(Strategy strategy, Field[,] fields)
     {
         bool found;
 
-        strategy1.Run(fields, potentialblock);
-        strategy2.Run(fields, fieldsperblock);
-        strategy3.Run(fields, fieldsperblock);
+        strategy.one.Run(fields, potentialblock);
+        strategy.two.Run(fields, fieldsperblock);
+        strategy.three.Run(fields, fieldsperblock);
         
-        found = strategy4.Run(fields);
+        found = strategy.four.Run(fields);
         if (found) return true;
 
-        found = strategy5.Run(fields, potentialblock);
+        found = strategy.five.Run(fields, potentialblock);
         if (found) return true;
         
-        found = strategy6.Run(fields, fieldsperblock);
+        found = strategy.six.Run(fields, fieldsperblock);
         if (found) return true;
 
-        found = strategy7.Run(fields, fieldsperblock);
+        found = strategy.seven.Run(fields, fieldsperblock);
         if (found) return true;
     
         return false;
